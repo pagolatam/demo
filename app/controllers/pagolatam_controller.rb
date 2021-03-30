1d3041f50754ff7fcd4b417479cc21451ebb95c0
chinoxchen <chienfu.udp@gmail.com>
2021-03-30 15:27:38 -0300

    fix notify callback

--- app/controllers/pagolatam_controller.rb
+++ app/controllers/pagolatam_controller.rb
@@ -1,31 +1,33 @@
 class PagolatamController < ApplicationController
+  protect_from_forgery except: [:notify]
+
   def notify
-    pl_service_token = params['pl_service_token']
-    pl_amount = params['pl_amount']
-    pl_currency = params['pl_currency']
-    pl_gateway_reference = params['pl_gateway_reference']
-    pl_order_id = params['pl_order_id']
-    pl_result = params['pl_result']
-    pl_timestamp = params['pl_timestamp']
-    pl_signature = params['pl_signature']
-    pl_session_id = params['pl_session_id']
+    payload = {
+      pl_service_token: params['pl_service_token'],
+      pl_amount: params['pl_amount'],
+      pl_currency: params['pl_currency'],
+      pl_gateway_reference: params['pl_gateway_reference'],
+      pl_order_id: params['pl_order_id'],
+      pl_result: params['pl_result'],
+      pl_timestamp: params['pl_timestamp'],
+      pl_session_id: params['pl_session_id'],
+      pl_signature: params['pl_signature']
+    }
 
-    payment = Payment.find_by!(token: pl_session_id)
-    order = Order.find_by!(token: pl_order_id)
-
-    create_signature = 'pl_amount'+pl_amount+
-                       'pl_currency'+pl_currency+
-                       'pl_gateway_reference'+pl_gateway_reference+
-                       'pl_order_id'+pl_order_id+
-                       'pl_result'+pl_result+
-                       'pl_session_id'+pl_session_id+
-                       'pl_service_token'+pl_service_token+
-                       'pl_timestamp'+pl_timestamp
+    create_signature = ''
+    payload.keys.sort.each do |key|
+      next if key.to_s == 'pl_signature'
+      create_signature += (key.to_s + payload[key].to_s)
+    end
 
     signature = OpenSSL::HMAC.hexdigest('sha256', ENV['PAGOLATAM_SECRET_TOKEN'], create_signature)
+    raise 'signature dont match' if signature != payload[:pl_signature]
 
-    if !payment.completed? && signature == pl_signature
-      case pl_result
+    payment = Payment.find_by!(token: payload[:pl_session_id])
+    order = Order.find_by!(token: payload[:pl_order_id])
+
+    if !payment.confirmed?
+      case payload[:pl_result]
       when 'completed'
         payment.confirmed!
         order.confirmed!
